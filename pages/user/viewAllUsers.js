2a7d3ca5a89b198a1e2c27b345e061af5c83dc8a
Eliot996 <math10t6@stud.kea.dk>
2022-11-24 18:52:34 +0100

    OSH-24 view all done

---
+++ pages/user/viewAllUsers.js
@@ -0,0 +1,64 @@
+const url = "http://localhost:8080/api/v1/user"
+
+let users = []
+
+async function getUsers() {
+    let response = await fetch(url)
+    users = Array.from(await response.json());
+
+    displayUsers();
+    edit(3); // debugging ------------------------------------------------
+}
+getUsers();
+
+function displayUsers() {
+    let tableBody = document.getElementById("user-body")
+    tableBody.innerHTML = "";
+
+    users.forEach(user => {
+        tableBody.innerHTML += (`<tr id="user${user.id}">
+        <th scope="row">${user.id}</th>
+        <td>${user.username}</td>
+        <td>**********</td>
+        <td >
+            <button id="edit" class="btn btn-info" onclick="edit(${user.id})">edit</button> 
+            <button id="delete" class="btn btn-danger" onclick="deleteUser(${user.id})">delete</button>
+        </td>
+      </tr>`)
+
+    });
+}
+
+function edit(id) {
+    // get user
+    const user = users.filter((u) => u.id == id)[0];
+
+    // get row
+    const row = document.getElementById("user" + id)
+
+    // get clone
+    const template = document.querySelector(`#template-edit-row`);
+    const clone = template.content.cloneNode(true);
+
+    // set info
+    clone.querySelector("#username-input").value = user.username;
+    clone.querySelector("#id").innerHTML = user.id;
+
+    // set eventlistners
+    clone.querySelector("#save").addEventListener("click", () => saveUser(user.id))
+    clone.querySelector("#cancel").addEventListener("click", () => displayUsers(user.id))
+    clone.querySelector("#delete").addEventListener("click", () => deleteUser(user.id))
+
+    // set clone
+    row.innerHTML = "";
+    row.appendChild(clone);
+}
+
+
+function saveUser(id) {
+    console.log("save for: " + id);
+}
+
+function deleteUser(id) {
+    console.log("delete for: " + id);
+}

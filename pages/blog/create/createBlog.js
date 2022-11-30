ed6521f1ad6242ecc609500ca3fc164f00fde403
Eliot996 <math10t6@stud.kea.dk>
2022-11-30 13:16:55 +0100

    OSH-37 edit added and working

--- pages/blog/create/createBlog.js
+++ pages/blog/create/createBlog.js
@@ -1,8 +1,19 @@
 import config from "../../../js/config.js"
 import { fileLoad } from "../../../js/utils.js";
-const url = config.url + "blogpost"
 
-export default async function initCreateBlog() {
+let url = config.url + "blogpost";
+let method = "POST";
+
+export default async function initCreateBlog(id) {
+    if (id != null && id > 0) {
+        method = "PATCH"
+        document.getElementById('submit-button').innerText = "Update Blog"
+        setblogData(id);
+        url += "/" + id
+    } else {
+        resetCreate();
+    }
+
     document.getElementById("submit-button").addEventListener("click", (event) => createBlog(event));
     document.getElementById("picture").addEventListener("input", () => loadAndPreviewFile())
 }
@@ -21,7 +32,7 @@ async function createBlog(event) {
     }
 
     let response = await fetch(url, {
-        method: "POST",
+        method: method,
         credentials: "include",
         headers: {'Content-Type': 'application/json'}, 
         body: JSON.stringify(data)
@@ -29,15 +40,27 @@ async function createBlog(event) {
     
 
     if (await response.ok) {
-        document.getElementById("title").value = "";
-        document.getElementById("text").value = "";
-        document.getElementById("picture").value = "";
-        document.getElementById('imagePreview').src = "";
+        resetCreate();
 
         window.location = "#/blog"
     } else {
         throw new Error("Could not send the info to the database");
     }
+}
+
+// function to helo edit to set values from the blog, based on id
+async function setblogData(id) {
+
+    let response = await fetch(url + "/" + id)
+    let blogPost = await response.json();
+
+    document.getElementById("title").value = blogPost.title;
+    document.getElementById("text").value = blogPost.text;
+    document.getElementById("picture").value = "";
+    document.getElementById('imagePreview').src = blogPost.picture;
+
+    document.getElementById('submit-button').innerText = "Update Blog"
+    document.getElementById('header').innerText = "Update blogpost"
 }
 
 // to load the image when the input field updates
@@ -48,4 +71,17 @@ async function loadAndPreviewFile() {
         fileAsBase64 = file;
         document.querySelector('#imagePreview').src = file
     });
+}
+
+// resets the create page to default
+function resetCreate() {
+    document.getElementById("title").value = "";
+    document.getElementById("text").value = "";
+    document.getElementById("picture").value = "";
+    document.getElementById('imagePreview').src = "";
+
+    document.getElementById('submit-button').innerText = "Create Blog"
+    document.getElementById('header').innerText = "Create new blogpost"
+    method = 'POST';
+    url = config.url + "blogpost";
 }

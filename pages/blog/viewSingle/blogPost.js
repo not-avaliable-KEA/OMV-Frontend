f07df2dc7eb82b0aa39910520d42fd207a42dd7b
H4ppyN4p <tobi5703@stud.kea.dk>
2022-12-02 11:36:27 +0100

    OSH-8 added edit/delete buttons in plogPost and changed layout

--- pages/blog/viewSingle/blogPost.js
+++ pages/blog/viewSingle/blogPost.js
@@ -21,23 +21,37 @@ async function getPostInfo(){
 function displayPost(){
 
     //Grab the div from the HTML Document we're gonna insert into later
-    let templateDiv = document.getElementById("blogPost")
+    let templateDiv = document.getElementById("blogPostContainer")
     templateDiv.innerHTML = ""
+
 
 
     //Grab all the data from the JSON
     let text = postData.text;
     let title = postData.title;
     let picture = postData.picture;
+    let date = postData.createdDate;
+
 
 
     //set the header
     let blogHeader = document.createElement("h1")
     blogHeader.innerHTML = title;
 
-    //set the text
-    let blogText = document.createElement("div")
-    blogText.innerHTML = text;
+    //Set the created date
+    let blogDate = document.createElement("div")
+    blogDate.innerHTML = date;
+    blogDate.className = "col-md-6"
+
+    //Set the edit/delete buttons
+    let editButton = document.createElement("button")
+    editButton.innerHTML = "edit"
+    editButton.style.visibility = "hidden"
+    editButton.className = "col-md-3 btn btn-info"
+    let deleteButton = document.createElement("button")
+    deleteButton.innerHTML = "delete"
+    deleteButton.style.visibility = "hidden"
+    deleteButton.className = "col-md-3 btn btn-danger"
 
     //set the picture
     let blogPicture = document.createElement("img")
@@ -45,6 +59,11 @@ function displayPost(){
     blogPicture.style.display = "blog"
     blogPicture.style.width = "100%"
 
+    //set the text
+    let blogText = document.createElement("div")
+    blogText.innerHTML = text;
+
+   
 
     //create the div for the blog-post
     let post = document.createElement("div")
@@ -53,30 +72,60 @@ function displayPost(){
     //create the header div
     let postHeader = document.createElement("div")
     postHeader.className = "col-12"
+    postHeader.className = "pt-3"
     postHeader.appendChild(blogHeader)
 
-    //create the div to hold the text 
-    let postText = document.createElement("div")
-    postText.className = "col-12"
-    postText.appendChild(blogText)
+    //create the div to hold date/time as well as edit/delete buttons
+    let postDate = document.createElement("div")
+    postDate.className = "col-12 row" 
+    postDate.appendChild(blogDate)
+    postDate.appendChild(editButton)
+    postDate.appendChild(deleteButton)
 
     //create the div to hold the image
     let postImage = document.createElement("div")
     postImage.className = "col-12"
     postImage.appendChild(blogPicture)
 
+     //create the div to hold the text 
+     let postText = document.createElement("div")
+     postText.className = "col-12"
+ 
+     postText.appendChild(blogText)
+
     //append all the elements to the main post element
+    //This decides the placement of hte elements
     post.appendChild(postHeader)
-    post.appendChild(postText)
+    post.appendChild(postDate)
     post.appendChild(postImage)
+    post.appendChild(postText)
 
     //insert the element into the page
     templateDiv.appendChild(post)
 
-    //replace
+    displayButtons(editButton, deleteButton);
 }   
+
+function displayButtons(editButton, deleteButton){
+
+    console.log("Testy testy")
+
+    if ((sessionStorage.getItem("username") != null && sessionStorage.getItem("username") !== "")
+    && (sessionStorage.getItem("userId") > 0 && sessionStorage.getItem("userId") != null)) {
+        // set link to edit and remove hidden
+        editButton.style.visibility = "visible";
+        editButton.addEventListener("click", () => document.location = "#/blog/" + postData.id + "/edit");
+        // set link to delete and remove hidden
+        deleteButton.style.visibility = "visible";
+        deleteButton.addEventListener("click", () => deletePost(postData.id));
+        
+        console.log("testy")
+    }
+}
 
 export default function initBlogPost() {
     //get which blog to show
     getPostInfo();
+    
+    
 }
